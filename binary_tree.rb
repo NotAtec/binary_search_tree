5a5c4393c3544bc4017943b88642991213d373b5
Jeroen B <jeroenb04@live.com>
2022-01-27 16:11:56 +0100

    Added #pretty_print for testing

--- binary_tree.rb
+++ binary_tree.rb
@@ -53,7 +53,12 @@ class Tree
       end
     end
   end
-
+  
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
 
 t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
