98efd443041351cc10070a9a615d58f8a8d1eb53
Jeroen B <jeroenb04@live.com>
2022-01-27 15:57:29 +0100

    Create #build_tree according to balanced spec

--- binary_tree.rb
+++ binary_tree.rb
@@ -2,6 +2,7 @@
 require 'pry-byebug'
 # Contains value & children
 class Node
+  include Comparable
   attr_accessor :data, :left, :right
 
   def initialize(d, l = nil, r = nil)
@@ -13,15 +14,27 @@ end
 
 # Contains root of tree & way of traversing it
 class Tree
-  include Comparable
   attr_accessor :root
 
   def initialize(arr)
-    @root = build_tree(arr)
+    @root = build_tree(arr.uniq.sort)
   end
 
   def build_tree(arr)
+    return Node.new(arr) if arr.length <= 1
+
+    middle = Node.new(arr[(arr.length / 2).floor])
+
+    left = build_tree(arr[0..(arr.length / 2).floor - 1])
+    right = build_tree(arr[(arr.length / 2).floor + 1..-1])
+
+    middle.left = left
+    middle.right = right
+    middle
   end
+
 end
+
+
 
 t = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
